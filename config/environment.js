c73e896e2deb51ad24b7c9c18b70ac2abd581179
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-02-26 12:36:20 +0100

    fix api host

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    host: 'http://subkitchen-api.herokuapp.com',
+    host: 'https://subkitchen-api.herokuapp.com',
     apiEndpoint: '/api/v1',
     modulePrefix: 'subkitchen-front',
     podModulePrefix: 'subkitchen-front/pods',
