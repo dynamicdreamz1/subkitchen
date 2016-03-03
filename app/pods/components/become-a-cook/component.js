537a7838b5f2d579b3449b004a7360a264509c1e
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-03 10:12:08 +0100

    #SK-102 "how to become a cook" section for unathorized user

--- app/pods/components/become-a-cook/component.js
+++ app/pods/components/become-a-cook/component.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
+  routing: Ember.inject.service('-routing'),
+
+  handle: null,
+
+  shouldSeeMe: Ember.computed('session', function(){
+    if (this.get('session').get('isAuthenticated')){
+      return !(this.get('session').get('data.user').artist &&
+               this.get('session').get('data.user').status === 'verified')
+    } else {
+      return true;
+    }
+  }),
+
+  didRender(){
+    if (!this.get('session').get('isAuthenticated')){
+      this.$('.become-a-cook').addClass('clickable');
+    }
+  },
+
+  actions: {
+    wholeBannerClick(){
+      if (!this.get('session').get('isAuthenticated')){
+        $('#loginModal').foundation('open');
+      }
+    },
+    join(){
+      if (!this.get('session').get('isAuthenticated')){
+        $('#loginModal').foundation('open');
+      } else {
+        this.get('routing').transferTo('become-cook');
+      }
+    }
+  }
 });
