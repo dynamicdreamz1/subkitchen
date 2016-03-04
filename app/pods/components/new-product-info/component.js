985441f87d7c6c446fe54fee57960da04e370444
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-04 09:59:57 +0100

    #SK-105 adjust product listings

--- app/pods/components/new-product-info/component.js
+++ app/pods/components/new-product-info/component.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  route: Ember.inject.service('-routing'),
+
+  actions: {
+    goToCooking(){
+      this.get('route').transitionTo('cooking');
+    }
+  }
 });
