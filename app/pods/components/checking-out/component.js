330a44706ec044c981e7370b6b06bc12b40d3177
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-08 15:41:01 +0100

    #SK-78 check out view

--- app/pods/components/checking-out/component.js
+++ app/pods/components/checking-out/component.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   cart: Ember.inject.service('shopping-cart'),
+  errors: {},
+  order: new Ember.Object({
+    shipping_method: 'paypal'
+  }),
 
   hasData: Ember.computed('cart.order.data', function(){
     let order = this.get('cart.order.data');
