887b54a5befae54e63929080320aec8888fbf40f
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-21 12:50:21 +0100

    #SK_100 promo boxes

--- app/pods/components/home-banner/component.js
+++ app/pods/components/home-banner/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
   routing: Ember.inject.service('-routing'),
+  promo: null,
 
   didInsertElement() {
     this.$().foundation();
