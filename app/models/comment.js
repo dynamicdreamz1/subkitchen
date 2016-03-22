dcdf4a6c9ee6c5422a9a180f6279d82c0127c2ee
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-22 14:27:13 +0100

    #SK-63 comments list

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,9 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  text:            DS.attr('string'),
-  created_at:      DS.attr('string'),
-  image_url:       DS.attr('string'),
-
-  product:         DS.belongsTo('product')
+  content:    DS.attr('string'),
+  createdAt:  DS.attr('date'),
+  user:       DS.belongsTo('user')
 });
--- app/models/product.js
+++ app/models/product.js
@@ -1,14 +1,15 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name:        DS.attr('string'),
-  description: DS.attr('string'),
-  shipping:    DS.attr('string'),
-  author:      DS.attr('string'),
-  image_url:   DS.attr('string'),
-  price:       DS.attr('number'),
-  likes_count: DS.attr('number'),
+  name:           DS.attr('string'),
+  description:    DS.attr('string'),
+  shipping:       DS.attr('string'),
+  author:         DS.attr('string'),
+  image_url:      DS.attr('string'),
+  price:          DS.attr('number'),
+  likes_count:    DS.attr('number'),
+  comments_count: DS.attr('number'),
 
-  promoters:   DS.hasMany('promoter'),
-  comments:    DS.hasMany('comment')
+  promoters:   DS.hasMany('promoter')
 });
