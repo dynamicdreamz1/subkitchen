1cca673e6e9ca192d0090e87125f20a8e25c49ff
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-25 14:07:03 +0100

    #SK-85 product creating tool page

--- app/pods/components/product-creator/component.js
+++ app/pods/components/product-creator/component.js
@@ -1,5 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  selectedTemplate: null,
+  size: 'MD',
+  quantity: 1,
+
+  init(){
+    this._super(...arguments);
+    this.set('selectedTemplate', this.get('productTemplates.firstObject'));
+  },
+
+  didInsertElement() {
+    this.$().foundation();
+  },
+
+  actions: {
+    selectTemplate(template){
+      this.set('selectedTemplate', template);
+    },
+
+    addToCart(){
+    },
+
+    decreaseQuantity(){
+      let newValue = this.get('quantity') - 1;
+      if (newValue < 1){
+        newValue = 1; }
+      this.set('quantity', newValue);
+    },
+
+    increaseQuantity(){
+      let newValue = this.get('quantity') + 1;
+      this.set('quantity', newValue);
+    },
+
+  }
 
 });
