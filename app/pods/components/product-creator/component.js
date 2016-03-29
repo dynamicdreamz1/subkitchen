71ca44c84969c5d556ae5af11b72c1958aadb265
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-03-29 14:12:00 +0200

    #SK-86 upload image on new product page

--- app/pods/components/product-creator/component.js
+++ app/pods/components/product-creator/component.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   selectedTemplate: null,
   size: 'MD',
   quantity: 1,
+  product: new Ember.Object(),
 
   init(){
     this._super(...arguments);
@@ -13,6 +14,20 @@ export default Ember.Component.extend({
   didInsertElement() {
     this.$().foundation();
   },
+
+  observeImage: function () {
+    if (this.get('product.image') && this.get('product.image').length){
+      let file = this.$('#imageFileUpload')[0].files[0];
+      let preview = this.$(".upload-preview");
+      let reader = new FileReader();
+      reader.onload = function(e){
+        let image_base64 = e.target.result;
+        preview.attr("src", image_base64);
+      };
+      reader.readAsDataURL(file);
+      preview.show();
+    }
+  }.observes('product.image'),
 
   actions: {
     selectTemplate(template){
