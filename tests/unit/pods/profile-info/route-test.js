077d84972dbca6df7fc1e1480b1ed19f1dccf4bc
klarahirao <klara.hirao@elpassion.pl>
2016-04-28 18:17:19 +0200

    Fix profile test

--- tests/unit/pods/profile-info/route-test.js
+++ tests/unit/pods/profile-info/route-test.js
@@ -1,6 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:profile', 'Unit | Route | profile', {
+moduleFor('route:profile/info', 'Unit | Route | profile', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
