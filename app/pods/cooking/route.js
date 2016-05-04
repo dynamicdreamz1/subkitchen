76e6de943ec211c26702236bfcfdfc0eac96b3f7
klarahirao <klara.hirao@elpassion.pl>
2016-05-04 14:01:46 +0200

    SK-92 create product: publish area for artist only

--- app/pods/cooking/route.js
+++ app/pods/cooking/route.js
@@ -3,11 +3,26 @@ import config from 'subkitchen-front/config/environment';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
+  session: Ember.inject.service('session'),
+
 
   model(){
+    let userPromise = new Ember.RSVP.Promise((resolve, reject) => {
+      if(this.get('session.isAuthenticated')){
+        this.store.findRecord('user', 'current').then(function(user){
+          resolve(user);
+        }, function(){
+          reject();
+        });
+      } else {
+        resolve({});
+      }
+    });
+
     return Ember.RSVP.hash({
       templates: this.store.query('productTemplate', {}),
-      themes: this.get('ajax').request(config.host + config.apiEndpoint + '/themes')
+      themes: this.get('ajax').request(config.host + config.apiEndpoint + '/themes'),
+      user: userPromise
     });
   }
 });
