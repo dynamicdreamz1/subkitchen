b10b3f5bb501051d851b4260eb8be4c2f4f98600
klarahirao <klara.hirao@elpassion.pl>
2016-05-10 19:33:26 +0200

    #SK-123 congratulations page: validation

--- app/pods/published-product/route.js
+++ app/pods/published-product/route.js
@@ -1,11 +1,24 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
   model(params){
+    let productPromise = new Ember.RSVP.Promise((resolve, reject) => {
+      this.set('product', this.store.findRecord('product', params.product_id)).then((product) => {
+        this.set('author', this.store.findRecord('user', 'current')).then(() => {
+          if(this.get('author.id') == this.get('product.author_id')) { // jshint ignore:line
+            resolve(product);
+          }
+          reject();
+        });
+      }, function(){
+        reject();
+      });
+    });
+
     return Ember.RSVP.hash({
-      product: this.store.findRecord('product', params.product_id)
+      product: productPromise
     });
   }
 });
