4e3997ff9ff136fad18e030560e4e1c673137a38
klarahirao <klara.hirao@elpassion.pl>
2016-05-13 16:54:08 +0200

    #SK-139 filtering products

--- app/pods/products/controller.js
+++ app/pods/products/controller.js
@@ -2,10 +2,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['page', 'perPage'],
+  queryParams: ['page', 'per_page', 'product_type', 'tags', 'price_range', 'sorted_by'],
 
   page: 1,
-  perPage: 28,
+  per_page: 12,
+  priceRanges: [[0, 25], [25, 100]],
+  product_type: [],
+  tags: [],
+  price_range: null,
+  sorted_by: 'created_at_desc',
+
+  productTypes: function(){
+    return this.get('model.templates').mapBy('product_type');
+  }.property('model.templates'),
 
   actions: {
     pageClicked(page) {
@@ -15,6 +24,14 @@ export default Ember.Controller.extend({
           scrollTop: $("#products").offset().top
         }, 500);
       }, 1000);
+    },
+
+    updateSortBy(sortBy){
+      this.set('sort_by', sortBy);
+    },
+
+    changePerPage(perPage) {
+      this.set('per_page', perPage);
     }
   }
 });
--- app/pods/products/route.js
+++ app/pods/products/route.js
@@ -1,8 +1,26 @@
 import Ember from 'ember';
 import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
+import config from 'subkitchen-front/config/environment';
 
 export default Ember.Route.extend(RouteMixin, {
-  model: function(params) {
-    return this.findPaged('product', params);
+  ajax: Ember.inject.service(),
+  queryParams: {
+    product_type: { refreshModel: true },
+    tags: { refreshModel: true },
+    price_range: { refreshModel: true },
+    sorted_by: { refreshModel: true },
+    per_page: { refreshModel: true }
+  },
+
+
+
+  model(params) {
+    params.paramMapping = { perPage: 'per_page' };
+
+    return Ember.RSVP.hash({
+      products: this.findPaged('product', params),
+      themes: this.get('ajax').request(config.host + config.apiEndpoint + '/themes'),
+      templates: this.store.query('productTemplate', {})
+    });
   }
 });
