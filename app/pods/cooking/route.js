0190a7501480bb9697d6a827bd465fe6e2c37825
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-05-18 12:36:58 +0200

    initialize product creator by product_id

--- app/pods/cooking/route.js
+++ app/pods/cooking/route.js
@@ -28,13 +28,13 @@ export default Ember.Route.extend({
     let productPromise = null;
     if(params.product_id) {
       productPromise = new Ember.RSVP.Promise((resolve, reject) => {
-        this.set('product', this.store.findRecord('product', params.product_id)).then((product) => {
+        this.set('product', this.store.findRecord('product', params.product_id)).then(() => {
           this.set('author', this.store.findRecord('user', 'current')).then(() => {
             if (this.get('author.id') == this.get('product.author_id')) { // jshint ignore:line
               let oReq = new XMLHttpRequest();
               oReq.open("GET", this.get('product.image_url'), true);
               oReq.responseType = "blob";
-              oReq.onload = (oEvent)=>{
+              oReq.onload = ()=>{
                 let blob = oReq.response;
                 this.set('product.image', blob);
                 resolve(this.get('product'));
