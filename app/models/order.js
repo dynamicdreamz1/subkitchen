9b2a8b82a2a978cc23c3df7190c6e3ddc50f6714
klarahirao <klara.hirao@elpassion.pl>
2016-05-23 23:59:58 +0200

    #SK-134 profile orders view

--- app/models/order.js
+++ app/models/order.js
@@ -1,10 +1,20 @@
 import DS from 'ember-data';
 
-
 export default DS.Model.extend({
-  uuid:        DS.attr('string'),
-  createdAt:   DS.attr('date'),
-  orderStatus: DS.attr('string'),
-  totalCost:   DS.attr('string'),
-  pdf:         DS.attr('string')
+  uuid:            DS.attr('string'),
+  createdAt:       DS.attr('date'),
+  status:          DS.attr('string'),
+  totalCost:       DS.attr('string'),
+  pdf:             DS.attr('string'),
+  purchasedAt:     DS.attr('date'),
+  subtotal:        DS.attr('string'),
+  shippingCost:    DS.attr('string'),
+  tax:             DS.attr('string'),
+  taxCost:         DS.attr('string'),
+  discount:        DS.attr('string'),
+  invoice:         DS.attr('string'),
+  placed:          DS.attr('string'),
+  items:           DS.attr(),
+  shippingAddress: DS.attr(),
+  deletedItems:    DS.attr()
 });
