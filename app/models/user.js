fc72a7d732fcbcec254d38914b403306a0c0ef8b
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-05-27 11:24:16 +0200

    SK-237 public profile page

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name:      DS.attr('string'),
-  lastName:  DS.attr('string'),
-  firstName: DS.attr('string'),
-  imageUrl:  DS.attr('string'),
-  email:     DS.attr('string'),
-  artist:    DS.attr('boolean'),
-  handle:    DS.attr('string'),
-  status:    DS.attr('string'),
+  name:           DS.attr('string'),
+  lastName:       DS.attr('string'),
+  firstName:      DS.attr('string'),
+  imageUrl:       DS.attr('string'),
+  email:          DS.attr('string'),
+  artist:         DS.attr('boolean'),
+  handle:         DS.attr('string'),
+  status:         DS.attr('string'),
+  products_count: DS.attr('number'),
 
   products:  DS.hasMany('products'),
 });
--- app/pods/public-profile/route.js
+++ app/pods/public-profile/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    return { handle: params.handle };
+    return this.store.queryRecord('user', { handle: params.handle });
   }
 });
