67d645b84055df346de2fb6ffade4311d0ab6215
klarahirao <klara.hirao@elpassion.pl>
2016-06-13 01:53:08 +0200

    #SK-256 newsletter

--- app/pods/components/app-footer/component.js
+++ app/pods/components/app-footer/component.js
@@ -1,7 +1,29 @@
+/* global $ */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  newsletterReceiverEmail: "",
+  errors: '',
+  newsletterReceiver: new Ember.Object(),
+
   didInsertElement() {
     this.$().foundation();
+  },
+
+  actions: {
+
+    subscribe() {
+      this.set('errors', '');
+      let newsletterReceiver = this.get('store').createRecord('newsletter-receiver', {
+        email: this.get('newsletterReceiverEmail')
+      });
+      newsletterReceiver.save().then(() => {
+        $('#newsletterModal').foundation('open');
+        this.set('newsletterReceiverEmail', "");
+      }, (error) => {
+        this.set('errors', error.errors[0].detail);
+      });
+    }
   }
 });
