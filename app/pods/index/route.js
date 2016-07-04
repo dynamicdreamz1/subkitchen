14f3998b51478398af4a368bcc6d81c31282e68e
klarahirao <klara.hirao@elpassion.pl>
2016-07-04 18:22:44 +0200

    #SK-279 featured products

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,14 +1,13 @@
-/* global $ */
 import Ember from 'ember';
 import config from 'subkitchen-front/config/environment';
 
 export default Ember.Route.extend({
   ajax: Ember.inject.service(),
 
-  model(params){
+  model(){
     return Ember.RSVP.hash({
       promo: this.get('ajax').request(config.host + config.apiEndpoint + '/config'),
-      trending: this.store.query('product', $.extend({page: 1, per_page: 4}, params))
+      trending: this.store.query('product', { page: 1, per_page: 4, featured: true })
     });
   }
 });
