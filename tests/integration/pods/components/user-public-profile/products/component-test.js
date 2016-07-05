5e8c1a79886f292e38a02742a302289e44114eeb
Rafał Lisowski <rafal.lisowski@elpassion.pl>
2016-07-05 15:19:30 +0200

    use format-number with fallback

--- tests/integration/pods/components/user-public-profile/products/component-test.js
+++ tests/integration/pods/components/user-public-profile/products/component-test.js
@@ -1,5 +1,5 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
+// import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('user-public-profile/products', 'Integration | Component | user public profile/products', {
   integration: true
@@ -9,7 +9,7 @@ test('it renders', function() {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{user-public-profile/products}}`);
+  // this.render(hbs`{{user-public-profile/products}}`);
 
   // assert.equal(this.$().text().trim(), '');
   expect(0);
