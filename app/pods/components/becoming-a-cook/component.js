0b9fcff55f322cc9d91279c383b05ef48428d6e6
Klara <klara.hirao@elpassion.pl>
2016-07-14 17:19:13 +0200

    #SK-285 create brand (with verification): action

--- app/pods/components/becoming-a-cook/component.js
+++ app/pods/components/becoming-a-cook/component.js
@@ -1,49 +1,36 @@
+/* global $ */
 import Ember from 'ember';
 import config from 'subkitchen-front/config/environment';
 
 export default Ember.Component.extend({
   session: Ember.inject.service('session'),
   routing: Ember.inject.service('-routing'),
-  isoCountries: Ember.inject.service('iso-countries'),
   flashMessages: Ember.inject.service(),
-
-
-  handle: null, // from component params or null
-
-  company: Ember.computed(['session', 'handle'], function(){
-    let user = this.get('currentUser.content');
-    let company = new Ember.Object(user.get('company'));
-    let handle = this.get('handle') || user.get('handle');
-    company.set('handle', handle);
-    return company;
-  }),
 
   errors: {},
 
-  private: Ember.computed('company.has_company', function(){
-    return !this.get('company.has_company');
-  }),
-
   actions: {
+    scrollUp() {
+      setTimeout(() => {
+        $('html, body').animate({
+          scrollTop: $('body').offset().top
+        }, 500);
+      }, 500);
+    },
+
     showLogin(){
       this.$('#passwordReminderModal').foundation('close');
       this.$('#loginModal').foundation('open');
     },
 
-    becomeCook(){
-      if (!this.get('company.terms')) {
-        return false; }
-      var params = this.get('company').getProperties('handle', 'has_company', 'company_name', 'address', 'city', 'zip', 'region', 'country');
-      params.return_path = '/profile';
-      params.handle = params.handle || '';
+    becomeCook() {
       this.get('session').authorize('authorizer:custom', (headerName, headerValue) => {
         var headers = {};
         headers[headerName] = headerValue;
         Ember.$.ajax({
           headers: headers,
           method: "POST",
-          url: config.host + config.apiEndpoint + '/account/simple_verification',
-          data: params
+          url: config.host + config.apiEndpoint + '/account/simple_verification'
         }).then(() => {
           this.set('errors', {});
           this.get('routing').transitionTo('profile');
