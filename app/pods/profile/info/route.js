eee55ee2a9d8ec3719fff83bfa20f3b12cd19f96
Klara <klara.hirao@elpassion.pl>
2016-07-14 17:23:02 +0200

    #SK-300 change 'become an artist' banner

--- app/pods/profile/info/route.js
+++ app/pods/profile/info/route.js
@@ -2,6 +2,14 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  renderTemplate() {
+    this.render();
+    this.render('components/user-profile-artist-button', {
+      into: 'profile',
+      outlet: 'artist-stats'
+    });
+  },
+
   model: function(){
     return Ember.RSVP.hash({
       address: this.store.findRecord('address', 'current')
